484bed8eb4318e03a251b3be094d8537982c6212
tylerciteline <149508873+tylerciteline@users.noreply.github.com>
2023-10-31 13:02:43 -0500

    Update streamlit_app.py
    added index for fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 

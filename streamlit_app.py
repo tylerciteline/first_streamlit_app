4ba5fe596bf182e1fd368ceeae6189d672039f9e
tylerciteline <149508873+tylerciteline@users.noreply.github.com>
2023-10-31 11:14:15 -0500

    Update streamlit_app.py
    added text lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')

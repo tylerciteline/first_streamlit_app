cef9d35129a2df8f6c704ffbbce60dc7a45626b9
tylerciteline <149508873+tylerciteline@users.noreply.github.com>
2023-10-31 12:58:54 -0500

    Update streamlit_app.py
    remove test

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-streamlit.text('verify')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas

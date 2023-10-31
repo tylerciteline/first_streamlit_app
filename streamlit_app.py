f68e6f337ead4b4ee4999f62b3a98b46615664c9
tylerciteline <149508873+tylerciteline@users.noreply.github.com>
2023-10-31 13:05:57 -0500

    Update streamlit_app.py
    Trying to fix apostrophe the learning guide did not explain how to fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Moms New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
